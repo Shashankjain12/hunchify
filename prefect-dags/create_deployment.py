674a7248123023f78400fed0766788ee7c31017d
61416 <shashank.jain@cars24.com>
2024-03-22 22:26:51 +0530

    Hunching the data

--- prefect-dags/create_deployment.py
+++ prefect-dags/create_deployment.py
@@ -2,8 +2,8 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/Shashankjain12/hunchify/tree/main/prefect-dags",
-        entrypoint="runner.py:run",
+        source="https://github.com/Shashankjain12/hunchify.git",
+        entrypoint="prefect-dags/runner.py:run",
     ).deploy(
         name="hunch-deployment",
         work_pool_name="my-managed-pool",
